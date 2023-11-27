6228f5f72ec53a296c4c7e2c4157091066e7a50f
Sky3008 <caothien3008@gmail.com>
2023-11-27 17:17:52 +0200

    created function for deleteuser, and updates userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,33 @@ router.get('/display/:id', async (req, res) => {
     res.render('display', { user });
 });
 
+// Delete user endpoint
+router.get('/delete/:id', async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.redirect('/users');
+});
 
+// Delete user route
+router.get('/delete', (req, res) => {
+    res.render('deleteuser');
+  });
+
+  router.post('/delete', async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      // Find and delete the user by username
+      const deletedUser = await User.findOneAndDelete({ name });
+
+      if (deletedUser) {
+        res.redirect('/users');
+      } else {
+        res.render('userNotFound', { name });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 module.exports = router;
